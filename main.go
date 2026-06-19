4df8876f39943b868191ce2d1c9cb69e99465376
xbuyan <kevobuyan@gmail.com>
2026-06-19 05:25:53 +0300

    add flexible methodMiddleware that accepts any HTTP method

--- main.go
+++ main.go
@@ -17,6 +17,16 @@ func sendError(w http.ResponseWriter, code int, message string) {
 	fmt.Fprintf(w, message)
 }
 
+func methodMiddleware(next http.HandlerFunc, allowedMethod string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != allowedMethod {
+			sendError(w, http.StatusMethodNotAllowed, "Method not allowed")
+			return
+		}
+		next(w, r)
+	}
+}
+
 // This is a handler — a function that runs when a request arrives
 // Every handler receives two things:
 // w = the writer, how you send data BACK to the client
@@ -33,23 +43,12 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		return // stops the function immediately else the code below would run
 	}
 
-	if r.Method != "GET" {
-
-		sendError(w, http.StatusMethodNotAllowed, "Method not allowed")
-		return
-	}
-
 	// set the staus code explicitly before writing the body
 	//Headers must be set before writing the body
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Server is alive")
 }
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != "GET" {
-		sendError(w, http.StatusMethodNotAllowed, "Method not allowed")
-		return
-	}
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "This is a GO server built by Lucciano")
@@ -58,8 +57,8 @@ func main() {
 
 	//Tell Go: When someone requests "/", run homeHandler
 
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/", methodMiddleware(homeHandler, "GET"))
+	http.HandleFunc("/about", methodMiddleware(aboutHandler, "GET"))
 
 	// Start listening on port 9090
 	// This line BLOCKS — meaning the program stays running, waiting
